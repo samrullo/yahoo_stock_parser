e212e991108a355accb656062851e3ef27b70134
Subkhon Amrulloev <amrulloev.subhon@gmail.com>
2025-10-25 12:57:19 +0900

    yahoo fundamentals parsing

--- jeq_stocks_workspace/view_save_fundamentals.py
+++ jeq_stocks_workspace/view_save_fundamentals.py
@@ -19,9 +19,6 @@ logging.basicConfig(
     handlers=[logging.FileHandler("dump_eq_prices.log"), logging.StreamHandler()],
 )
 
-start = datetime.date(2025, 10, 21)
-end = datetime.date(2025, 10, 24)
-
 engine = create_engine(Config.db_uri)
 meta = MetaData()
 eq_px_tbl = Table("eq_prices", meta, autoload_with=engine)
@@ -38,13 +35,23 @@ logging.info(
     f"By New Index Series Code count\n{topix_df.groupby('New Index Series Code').count()['Issue']}"
 )
 
-topix_eq_px_df_list = []
+# query all fundamentals
+fundamentals_table_name = "yahoo_fundamentals"
+fundamdf = pd.read_sql(f"SELECT * FROM `{fundamentals_table_name}`", engine)
+logging.info(f"There are {len(fundamdf)} records in {fundamentals_table_name} table")
 
-tickers = topix_df["ticker"].unique().tolist()
-
-with tqdm(total=len(tickers)) as pbar:
-    for i, ticker in enumerate(tickers):
-        pbar.set_description(f"{i} : {ticker} start processing ....")
-        run_from_live(ticker, Config.db_uri)
-        pbar.update(1)
-
+# save fundamentals data
+save_results = False
+if save_results:
+    thefolder = pathlib.Path(r"/Users/samrullo/programming/pyprojects/yahoo_stock_parser/datasets")
+    fmrgdf = pd.merge(left=fundamdf, right=topix_df, on="ticker")
+    front_cols = get_list_diff(topix_df.columns, ["ticker", "Date"])
+    back_cols = get_list_diff(fundamdf.columns, ["ticker"])
+    fmrg_cols = ["ticker"] + front_cols + back_cols
+    tfdf = fmrgdf[fmrg_cols].copy()
+    tfdf = tfdf.rename(columns={"Component Weight (TOPIX)": "weight_pct"})
+    tfdf = remove_nonnumeric_chars_from_numeric_cols(tfdf, ["%"], ["weight_pct"])
+    tfdf = convert_columns_to_numeric(tfdf, ["weight_pct"])
+    tfdf = tfdf.sort_values("weight_pct", ascending=False)
+    tfdf["weight_pct_cum"] = tfdf["weight_pct"].cumsum()
+    tfdf.to_csv(thefolder / "topix_fundamentals.csv", index=False)
